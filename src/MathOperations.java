8b0e96d7519487c335278108710a089181e76336
elmartinyo <martins.ceske@gmail.com>
2020-02-10 22:59:04 +0200

    lesson2 labots

--- src/MathOperations.java
+++ src/MathOperations.java
@@ -1,15 +1,22 @@
+//ieliec majas darbus atsevišķā "package" - lv.javaguru.homework.lesson2
 public class MathOperations {
     public static void main(String[] args) {
         int firstNumber = 10;
         int secondNumber = 5;
+
+        //ieliekot atstarpi būs ērtak izlasīt
         int sumResult = firstNumber + secondNumber;
         int subtractResult = firstNumber - secondNumber;
         int divideResult = firstNumber / secondNumber;
         int multiplyResult = firstNumber * secondNumber;
+
+        //atstarpe vajadzīgā vietā kodu sadala "loģiskos blokos"
         System.out.println("Summa:" + sumResult);
         System.out.println("Atņemšanas rezultāts:" + subtractResult);
         System.out.println("Dalīšanā rezultāts:" + divideResult);
         System.out.println("Reizināšanas rezultāts:" + multiplyResult);
+
+        //šis nebija prasīts majas darbā - nav nepieciešams
         double a = Math.random() * 200 - 100;
         System.out.println(a);
     }
--- src/RandomNumbersServiceTest.java
+++ src/RandomNumbersServiceTest.java
@@ -1,13 +1,18 @@
+//ieliec majas darbus atsevišķā "package" - lv.javaguru.homework.lesson2
 import java.util.Random;
 
 public class RandomNumbersServiceTest {
     public static void main(String[] args) {
-        Random randomGenerator = new Random();
-        int firstRandomNumber = randomGenerator.nextInt(10);
 
+        //atstarpe vajadzīgā vietā kodu sadala "loģiskos blokos"
+        Random randomGenerator = new Random();
+
+        int firstRandomNumber = randomGenerator.nextInt(10);
         int secondRandomNumber = randomGenerator.nextInt(10);
         int thirdRandomNumber = randomGenerator.nextInt(10);
+
         int mySUM = firstRandomNumber+secondRandomNumber+thirdRandomNumber;
+
         System.out.print(firstRandomNumber+"+");
         System.out.print(secondRandomNumber+"+");
         System.out.print(thirdRandomNumber+"+");
