cfeed87544c57a3041b2521c75ca764390f7bdb5
Amn3z1ja <37236349+Amn3z1ja@users.noreply.github.com>
2020-03-14 20:44:18 +0200

    commit 7

---
+++ src/lv/javaguru/homework/lesson5/Book.java
@@ -0,0 +1,53 @@
+package lv.javaguru.homework.lesson5;
+
+public class Book {
+    private String author;
+    private String title;
+    private int pageCount;
+
+    public  String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title ;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+   @Override
+    public boolean equals(Object that) {
+        if (this == that) return true;
+        if (that == null ||  getClass() != that.getClass()) return false;
+        Book myBook = (Book) that;
+        boolean isAuthorEquals = myBook.author.equals(author);
+        boolean isTitleEquals = myBook.title.equals(title);
+        boolean isPageCountEquals = myBook.pageCount == pageCount;
+
+        return  isAuthorEquals &&
+                isTitleEquals &&
+                isPageCountEquals;
+
+//        return Object.equals(author,myBook.author) &&
+//                Object.equals(title,myBook.title) &&
+//                Object.equals(pageCount,myBook.pageCount);
+   }
+    @Override
+    public String toString(){
+        return "Book{" + "author=" +author + '/' +
+        ", title=" + title +'/' +
+    ". pageCount=" + pageCount+ '}';
+    }
+    }
+
+
