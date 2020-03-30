5ac2823df5059a826c1d3fe7110631ddf82ca942
elmartinyo <martins.ceske@gmail.com>
2020-03-30 23:50:26 +0300

    hw7 daļēji izpildīts, pievieno testus

--- src/lv/javaguru/homework/lesson7/ArrayService.java
+++ src/lv/javaguru/homework/lesson7/ArrayService.java
@@ -1,8 +1,12 @@
 package lv.javaguru.homework.lesson7;
 
 import java.util.Random;
-
+//šim trūkst testu vai darbības demonstrācijas
 public class ArrayService {
+
+    //šai metodei ir tikai jāizveido jauns masīvs un jāatgriež rezuktātā
+    //print nav nepieciešams
+    //Meotei kā parametru ir jānodod vēlamā masīva izmērs
     public int[] create(int[] n) {
         for (int i = 0; i < n.length; i = i + 2) {
             System.out.println(n);
