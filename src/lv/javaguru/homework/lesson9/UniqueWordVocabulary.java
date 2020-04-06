7ec7f0721bb76ce138f06892df1bf3b5aa17807f
elmartinyo <martins.ceske@gmail.com>
2020-04-07 00:07:02 +0300

    hw9, ieskaitīts - pievieno unit testu

--- src/lv/javaguru/homework/lesson9/UniqueWordVocabulary.java
+++ src/lv/javaguru/homework/lesson9/UniqueWordVocabulary.java
@@ -3,25 +3,21 @@ package lv.javaguru.homework.lesson9;
 import java.util.HashSet;
 import java.util.Set;
 
+//trūkst testu
 public class UniqueWordVocabulary {
-    Set<String> word = new HashSet<>();
 
+    Set<String> word = new HashSet<>();  //stāvoklim jabūt private
 
     public boolean addWords(String newWord) {
         return newWord != null && word.add(newWord);
     }
 
-
-
     public int getUniqueWords() {
         return word.size();
-
     }
 
     public void printToConsole() {
         word.forEach(System.out::println);
-
     }
-
 
 }
